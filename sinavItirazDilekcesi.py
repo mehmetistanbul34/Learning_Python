23d78659689e8576adae6ad41a561aac6764af34
Mehmet <tr.mehmetcakmak@gmail.com>
2019-12-30 01:28:49 +0300

    [MÇakmak], Dilekçe projesine bazı eklemeler yapıpldı

--- sinavItirazDilekcesi.py
+++ sinavItirazDilekcesi.py
@@ -7,17 +7,34 @@ dilekce = """
 	Fakülteniz {} Bölümü {} numaralı öğrencisiyim. Ekte sunduğum belgede belirtilen mazeretim 
 gereğince {} Eğitim-Öğretim Yılı {}. yarıyılında öğrenime ara izni (kayıt dondurma) istiyorum.
 Bilgilerinizi ve gereğini arz ederim.
+
+	İmza
+Ad: {}
+Soyad: {}
+T.C. Kimlik No. : {}
+Adres: {}
+Tel.: {}
+Ekler: {}
 """
 
-tarih = str(input("Tarihi giriniz: "))
-uniName = str(input("Üniversitenizin adını giriniz: "))
-fName = str(input("Fakültenizin adını giriniz: "))
-bolum = str(input("Bölüm adını giriniz: "))
-no = str(input("Numaranızı giriniz: "))
-yil = str(input("Eğitim-Öğretim Yılı giriniz: "))
-yariYil = str(input("Yarıyılı giriniz: "))
+tarih         = input("tarih: ")
+universite    = input("üniversite adı: ")
+fakulte       = input("fakülte adı: ")
+bolum         = input("bölüm adı: ")
+ogrenci_no    = input("öğrenci no. :")
+ogretim_yili  = input("öğretim yılı: ")
+yariyil       = input("yarıyıl: ")
+ad            = input("öğrencinin adı: ")
+soyad         = input("öğrencinin soyadı: ")
+tc_kimlik_no  = input("TC Kimlik no. :")
+adres         = input("adres: ")
+tel           = input("telefon: ")
+ekler         = input("ekler: ")
 
 import os
 os.system('clear')
 
-print(dilekce.format(tarih,uniName,fName,bolum,no,yil,yariYil))
+print(dilekce.format(tarih, universite, fakulte, bolum,
+ogrenci_no, ogretim_yili, yariyil,
+ad, soyad, tc_kimlik_no,
+adres, tel, ekler))
